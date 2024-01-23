ea6b1b5f3ffdc2144522d7a221ef1e5cd8ab5f96
marcinskubis <marcin.skubis@interia.eu>
2024-01-23 20:27:21 +0100

    init

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfTimes) {
+    if(string){
+        if(numOfTimes < 0) return "ERROR";
+        return string.repeat(numOfTimes);
+    }else{
+        return "";
+    }
 };
 
 // Do not edit below this line
